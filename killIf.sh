c3ed5185acea3d6c262613eb5747a8bbf7f8ee82
GaurangTandon <1gaurangtandon@gmail.com>
2019-06-29 19:37:03 +0530

    add grep process name

--- killIf.sh
+++ killIf.sh
@@ -3,6 +3,9 @@
 # source
 # https://askubuntu.com/q/54747
 
+# $1 is process name
+# $2 is memory limit in MB
+
 if [ $# -ne 2 ];
 then
     echo "Invalid number of arguments"
@@ -11,18 +14,21 @@ fi
 
 while true;
 do
-    SIZE=$(pmap $1|grep total|grep -o "[0-9]*")
-    SIZE=${SIZE%%K*}
-    SIZEMB=$((SIZE/1024))
-    echo "Process id =$1 Size = $SIZEMB MB"
-    if [ $SIZEMB -gt $2 ]; then
-        printf "SIZE has exceeded.\nKilling the process......"
-        kill -9 "$1"
-        echo "Killed the process"
-        exit 0
-    else
-        echo "SIZE has not yet exceeding"
-    fi
+    pgrep "$1" | while read -r procId;
+    do
+        SIZE=$(pmap $procId | grep total | grep -o "[0-9]*")
+        SIZE=${SIZE%%K*}
+        SIZEMB=$((SIZE/1024))
+        # echo "Process id = $procId Size = $SIZEMB MB"
+        if [ $SIZEMB -gt $2 ]; then
+            printf "SIZE has exceeded.\nKilling the process......"
+            kill -9 "$procId"
+            echo "Killed the process"
+            exit 0
+        # else
+            # echo "SIZE has not yet exceeding"
+        fi
+    done
 
-    sleep 10
+    sleep 1
 done
